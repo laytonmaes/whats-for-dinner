51ace257d809046b40cca36514413d99f6206186
Dylan Duke <laytonmaes@gmail.com>
2022-02-18 11:19:08 -0700

    Add event listeners for checkboxes, add randomize selected dish function and display the dish, complete functionality of code base

--- main.js
+++ main.js
@@ -1,23 +1,57 @@
 
 var buttonLetsCook = document.querySelector("#lets-cook")
-var buttonAddNew = document.querySelector("#add-new")
+
+var sectionDisplayMeal = document.querySelector(".meal-display")
+
+var checkboxSide = document.querySelector("#dish1")
+var checkboxMain = document.querySelector("#dish2")
+var checkboxDessert = document.querySelector("#dish3")
 
 var currentMeal
-buttonLetsCook.addEventListener("click", function(){
-  randomizeMeal()
-});
 
-buttonAddNew.addEventListener("click", function(){
-  event.preventDefault();
-  randomizeMeal()
+buttonLetsCook.addEventListener("click", randomizeSelectedDish);
+
+checkboxSide.addEventListener("click", function(){
+  selectOnlyThis(checkboxSide)
+})
+checkboxMain.addEventListener("click", function(){
+  selectOnlyThis(checkboxMain)
+} )
+checkboxDessert.addEventListener("click", function(){
+  selectOnlyThis(checkboxDessert)
 })
 
-function randomizeMeal(){
-  currentMeal = new Meal( sideDishes[randomizeIndex(sideDishes)],
-  mainDishes[randomizeIndex(mainDishes)],
-  dessertDishes[randomizeIndex(dessertDishes)]
-)
-console.log(currentMeal)
+// function randomizeMeal(){
+//   currentMeal = new Meal( sideDishes[randomizeIndex(sideDishes)],
+//   mainDishes[randomizeIndex(mainDishes)],
+//   dessertDishes[randomizeIndex(dessertDishes)]
+// )
+//
+// }
+function selectOnlyThis(dish){
+  for(var i = 1; i <= 3; i++) {
+    document.querySelector(`#${"dish" + i}`).checked = false;
+  }
+  dish.checked = true;
+}
+
+function randomizeSelectedDish(){
+  if(checkboxSide.checked){
+    var currentMeal = sideDishes[randomizeIndex(sideDishes)]
+  } else if(checkboxMain.checked){
+    var currentMeal = mainDishes[randomizeIndex(mainDishes)]
+  } else if(checkboxDessert.checked){
+    var currentMeal = dessertDishes[randomizeIndex(dessertDishes)]
+  } else {
+    return
+  }
+
+  sectionDisplayMeal.innerHTML = `<h2>${currentMeal}</h2>`
+}
+
+function displayMeal() {
+  sectionDisplayMeal.innerHTML = `<h2>${currentMeal}</h2>`
+  console.log(checkboxSide.checked)
 }
 
 function randomizeIndex(array){
